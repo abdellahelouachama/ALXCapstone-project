047c7f6a27f88bc0f6c7be0a8b384c84145c94db
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-12-11 15:35:36 +0100

    Socail Media API

--- Social_Media_API/api/urls.py
+++ Social_Media_API/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import UserAPIView
 
 
-# url patterns for the user views
+# url patterns for the user viewset
 urlpatterns = [
-    # this url pattern handle all the user operation(retrieve, update, delete)
+    # this url pattern handle all the user operations(retrieve, partial update, update, delete)
     # and based on the request method the appropriate view will be called (GET, PUT, PATCH, DELETE)
     path('users/<username>', UserAPIView.as_view(), name='user')
 ]
