9de8729b7d3cd4ecb279322f8f2a3c73d72b266c
abdellah el ouachama <abdellahelouchaa@gmail.com>
2024-12-22 18:19:46 +0100

    social media api

--- Social_Media_API/notification/pagination.py
+++ Social_Media_API/notification/pagination.py
@@ -1,3 +1,24 @@
+"""
+pagination.py
+
+This module defines a custom pagination class for the Social Media API.
+
+Classes:
+    CustomPagination(PageNumberPagination): A custom pagination class that sets the default page size.
+
+Usage:
+    This class can be used to paginate querysets in Django REST Framework views by setting it as the pagination class.
+
+Example:
+    from .pagination import CustomPagination
+    from rest_framework.generics import ListAPIView
+
+    class MyListView(ListAPIView):
+        queryset = MyModel.objects.all()
+        serializer_class = MyModelSerializer
+        pagination_class = CustomPagination
+"""
+
 from rest_framework.pagination import PageNumberPagination
 
 # Custom pagination
